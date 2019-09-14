66964b6beab6c7847da5b86686b65c53cb74edf5
Shintani Teppei <teppest1215@gmail.com>
2019-09-14 09:18:34 +0900

    raise InvalidArgument

--- app/servers/salary_server.rb
+++ app/servers/salary_server.rb
@@ -2,6 +2,8 @@ class SalaryServer < Salary::SalaryService::Service
   # @param request [SalaryPb::GetSalaryRequest]
   # @return [SalaryPb::GetSalaryResponse]
   def get_salary(request, _call)
+    raise GRPC::InvalidArgument unless request.user_id > 0
+
     user = User.find(request.user_id)
 
     SalaryPb::GetSalaryResponse.new(
@@ -12,6 +14,8 @@ class SalaryServer < Salary::SalaryService::Service
   # @param request [SalaryPb::ListSalariesRequest]
   # @return [SalaryPb::ListSalariesResponse]
   def list_salary(request, _call)
+    raise GRPC::InvalidArgument unless request.company_id > 0
+
     company = Company.find(request.company_id)
 
     SalaryPb::ListSalariesResponse.new(
@@ -22,6 +26,8 @@ class SalaryServer < Salary::SalaryService::Service
   # @param request [SalaryPb::DoubleBonusRequest]
   # @return [Google::Protobuf::Empty]
   def double_bonus(request, _call)
+    raise GRPC::InvalidArgument unless request.user_id > 0
+
     user = User.find(request.user_id)
     user.double_bonus!
 
@@ -31,6 +37,9 @@ class SalaryServer < Salary::SalaryService::Service
   # @param request [SalaryPb::UpdateMonthlySalaryRequest]
   # @return [Google::Protobuf::Empty]
   def update_monthly_salary(request, _call)
+    raise GRPC::InvalidArgument unless request.user_id > 0
+    raise GRPC::InvalidArgument unless request.monthly > 0
+
     user = User.find(request.user_id)
     user.update!(monthly: request.monthly)
 
