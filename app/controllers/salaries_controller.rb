fe277c07fdeacd2f63b2aaf3130241801cfe7b8c
Shintani Teppei <teppest1215@gmail.com>
2019-09-16 12:46:51 +0900

    fix

--- app/controllers/salaries_controller.rb
+++ app/controllers/salaries_controller.rb
@@ -1,6 +1,7 @@
 class SalariesController < ApplicationController
   def get_salary
     req = SalaryPb::GetSalaryRequest.decode(request.body.read)
+    authorize(req)
     res = SalaryServer.new.get_salary(req, nil)
 
     render protobuf: res
@@ -8,6 +9,7 @@ class SalariesController < ApplicationController
 
   def list_salary
     req = SalaryPb::ListSalariesRequest.decode(request.body.read)
+    authorize(req)
     res = SalaryServer.new.list_salary(req, nil)
 
     render protobuf: res
@@ -15,6 +17,7 @@ class SalariesController < ApplicationController
 
   def double_bonus
     req = SalaryPb::DoubleBonusRequest.decode(request.body.read)
+    authorize(req)
     req = SalaryServer.new.double_bonus(req, nil)
 
     render protobuf: res
@@ -22,6 +25,7 @@ class SalariesController < ApplicationController
 
   def update_monthly_salary
     req = SalaryPb::UpdateMonthlySalaryRequest.decode(request.body.read)
+    authorize(req)
     req = SalaryServer.new.update_monthly_salary(req, nil)
 
     render protobuf: res
